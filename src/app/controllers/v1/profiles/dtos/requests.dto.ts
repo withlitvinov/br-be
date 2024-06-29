f6999329770bd8d2d3ffa9b154c59f365d1f7e29
Anatolii Litvinov <krkma@turbo-machine.local>
2024-06-29 21:55:31 +0300

    feat: add descriptions

--- src/app/controllers/v1/profiles/dtos/requests.dto.ts
+++ src/app/controllers/v1/profiles/dtos/requests.dto.ts
@@ -2,16 +2,19 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class BirthdayDto {
   @ApiProperty({
+    description: 'A day of birth',
     example: 14,
   })
   day: number;
 
   @ApiProperty({
+    description: 'A month of birth',
     example: 3,
   })
   month: number;
 
   @ApiProperty({
+    description: 'A year of birth',
     example: 1998,
     nullable: true,
   })
@@ -21,6 +24,7 @@ export class BirthdayDto {
 export class CreateOneProfileRequestDto {
   @ApiProperty({
     example: 'Garry',
+    description: "Person's name",
   })
   name: string;
 
--- src/app/controllers/v1/profiles/dtos/responses.dto.ts
+++ src/app/controllers/v1/profiles/dtos/responses.dto.ts
@@ -1,26 +1,50 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class GetManyProfilesResponseDto {
-  @ApiProperty()
+  @ApiProperty({
+    type: 'uuid',
+    description: 'Id of the profile',
+    example: 'f537d724-393b-469c-87f5-e6bcd8d601bb',
+  })
   id: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: "Person's name",
+    example: 'Garry',
+  })
   name: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    description:
+      'A formatted date of birth. Example: Full date - 1998-06-20; Without year - 06-20',
+    example: '1998-06-20',
+  })
   birthday: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    description:
+      'Indicates date integrity, true value means that birth date with year',
+    example: true,
+  })
   is_full: boolean;
 }
 
 export class GetByIdProfileResponseDto {
-  @ApiProperty()
+  @ApiProperty({
+    description: 'Id of the profile',
+    example: 'f537d724-393b-469c-87f5-e6bcd8d601bb',
+  })
   id: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: "Person's name",
+    example: 'Garry',
+  })
   name: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: 'A formatted date of birth',
+    example: '1998-06-20',
+  })
   birthday: string;
 }
